c70c8f98d065dcb18a2d73a503554db94186f6ee
Adrian Guerrero <guerrero1111@hotmail.com>
2018-08-28 12:50:14 -0500

    cambio

--- platforms/android/cordova/lib/builders/StudioBuilder.js
+++ platforms/android/cordova/lib/builders/StudioBuilder.js
@@ -23,7 +23,6 @@ var util = require('util');
 var path = require('path');
 var shell = require('shelljs');
 var spawn = require('cordova-common').superspawn.spawn;
-var events = require('cordova-common').events;
 var CordovaError = require('cordova-common').CordovaError;
 var check_reqs = require('../check_reqs');
 
@@ -168,7 +167,7 @@ StudioBuilder.prototype.prepBuildFiles = function () {
         }
     };
     subProjects.forEach(function (p) {
-        events.emit('log', 'Subproject Path: ' + p);
+        console.log('Subproject Path: ' + p);
         var libName = p.replace(/[/\\]/g, ':').replace(name + '-', '');
         if (libName !== 'app') {
             depsList += '    implementation(project(path: ":' + libName + '"))';
