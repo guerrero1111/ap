ad480012b4df134da6d8ed156a6f4305ac91a3c5
Adrian Guerrero <guerrero1111@hotmail.com>
2018-08-28 10:44:34 -0500

    cambios

--- www/app.js
+++ www/app.js
@@ -575,6 +575,34 @@ $(".guardabtna").on( "click", function() {
 
 
 
+    document.getElementById("downloadFile").addEventListener("click", downloadFile);
+
+function downloadFile() {
+   var fileTransfer = new FileTransfer();
+   var uri = encodeURI("http://s14.postimg.org/i8qvaxyup/bitcoin1.jpg");
+   var fileURL =  "///storage/emulated/0/DCIM/myFile";
+
+   fileTransfer.download(
+      uri, fileURL, function(entry) {
+         console.log("download complete: " + entry.toURL());
+      },
+        
+      function(error) {
+         console.log("download error source " + error.source);
+         console.log("download error target " + error.target);
+         console.log("download error code" + error.code);
+      },
+        
+      false, {
+         headers: {
+            "Authorization": "Basic dGVzdHVzZXJuYW1lOnRlc3RwYXNzd29yZA=="
+         }
+      }
+   );
+}
+
+
+   
 
 // $("#uname").keypress(function (e) {
 //      if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
